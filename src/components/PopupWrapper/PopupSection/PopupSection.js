f282d5a72f15822eb87ac418382031a581a2df0c
leontaolong <longt8@uw.edu>
2019-05-21 14:09:27 -0700

    initial commit

---
+++ src/components/PopupWrapper/PopupSection/PopupSection.js
@@ -0,0 +1,7 @@
+import * as React from './node_modules/react';
+
+class PopupSection extends React.Component {
+    render() {
+    return 
+    }
+   }
---
+++ src/components/PopupWrapper/PopupWrapper.js
@@ -0,0 +1,17 @@
+import * as React from './node_modules/react';
+
+class PopupWrapper extends React.Component {
+    render() {
+    return 
+    /*
+        return (
+        <div>
+            <PopupTitle />
+            <CancelButton  onclick= '' />
+            <PopupSections>
+            <OptionalActions>
+        </div>
+      );
+    */
+    }
+   }
